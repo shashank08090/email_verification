7507474f1e1bce4547bedd74618e67a6522f0814
shashank <Shashank08090@gmail.com>
2025-09-29 13:18:04 +0530

    no security

--- src/main/java/com/spring/verification/springbackendverification/SpringBackendVerificationApplication.java
+++ src/main/java/com/spring/verification/springbackendverification/SpringBackendVerificationApplication.java
@@ -3,13 +3,15 @@ package com.spring.verification.springbackendverification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan("com.spring.verification.springbackendverification")
+@ComponentScan(basePackages = "com.spring.verification.springbackendverification",
+		excludeFilters = @Filter(type = FilterType.REGEX, pattern = "com.spring.verification.springbackendverification.security.*"))
 public class SpringBackendVerificationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendVerificationApplication.class, args);
 	}
-
 }
